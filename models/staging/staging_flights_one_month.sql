2c6a7ffe1cc22b4f15cb76b50b7b0a350b3b6ab9
vladislavkivaev <kivaevv@gmail.com>
2026-04-16 15:14:39 +0200

    updates

--- models/staging/staging_flights_one_month.sql
+++ models/staging/staging_flights_one_month.sql
@@ -3,6 +3,6 @@
 WITH flights_one_month AS (
     SELECT * 
     FROM {{source('flights_data', 'flights')}}
-    WHERE DATE_PART('month', flight_date) = 1 
+    WHERE DATE_PART('month', flight_date) = 9
 )
 SELECT * FROM flights_one_month
